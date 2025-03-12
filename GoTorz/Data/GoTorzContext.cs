9e6e2087fbde3f3e8dcda894753690e9f407c4b1
nicolai jensen <nbje63269@edu.ucl.dk>
2025-03-12 14:36:27 +0100

    scaffolded item med Entityframework, for fuld CRUD og databaseadgang

---
+++ GoTorz/Data/GoTorzContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GoTorz.Model;
+
+namespace GoTorz.Data
+{
+    public class GoTorzContext : DbContext
+    {
+        public GoTorzContext (DbContextOptions<GoTorzContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Package> Package { get; set; } = default!;
+    }
+}
--- GoTorz/Program.cs
+++ GoTorz/Program.cs
@@ -1,15 +1,23 @@
 using GoTorz.Components;
-using GoTorz.Services;
+
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GoTorz.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<GoTorzContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GoTorzContext") ?? throw new InvalidOperationException("Connection string 'GoTorzContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Register the PackageService with the connection string
-builder.Services.AddSingleton(new PackageService(builder.Configuration.GetConnectionString("DefaultConnection")));
+//// Register the PackageService with the connection string
+//builder.Services.AddSingleton(new PackageService(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 
@@ -22,6 +30,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
