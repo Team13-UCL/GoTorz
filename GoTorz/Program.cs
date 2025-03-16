5a19c14b0ced4f61bea0278a43fdf0ffa902e8a2
Mikkel <mikk203i@gmail.com>
2025-03-16 20:00:42 +0100

    Tilføjede Serilog, ændrede Weather.razor til Flights.razor

--- GoTorz/Program.cs
+++ GoTorz/Program.cs
@@ -1,6 +1,21 @@
 using GoTorz.Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Konfigurer Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()  // Logger til konsollen
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day) // Logger til en fil pr. dag
+    .CreateLogger();
+
+// Tilføj Serilog som Logger
+builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
