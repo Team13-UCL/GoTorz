92d0cdc41f56c8905e102bb0f2b6337ab8af8c55
Nicolai.Tofteby <nicolaitofteby@gmail.com>
2025-04-01 17:30:46 +0200

    Updated Login information + UI update for Register

--- GoTorz/Data/AuthContext.cs
+++ GoTorz/Data/AuthContext.cs
@@ -21,10 +21,18 @@ namespace GoTorz.Data
         {
             modelBuilder.Entity<User>(entity =>
             {
-                entity.HasKey(e => e.Email);
+                entity.HasKey(e => e.UserID); // Set UserID as the Primary Key
 
                 entity.ToTable("User");
 
+                entity.Property(e => e.UserID)
+                .ValueGeneratedOnAdd(); // ID is Auto Incremented when making a new User
+                entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+                entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
                 entity.Property(e => e.Email)
                     .HasMaxLength(100)
                     .IsUnicode(false);
--- GoTorz/Data/User.cs
+++ GoTorz/Data/User.cs
@@ -5,6 +5,9 @@ namespace GoTorz.Data
 {
     public partial class User
     {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
         public string Password { get; set; }
