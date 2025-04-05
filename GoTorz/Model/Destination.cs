a12d918c808edd338d2b211e780829fe33c00728
Sami <seel63284@edu.ucl.dk>
2025-04-05 21:19:31 +0200

    Kundeside og homeside

---
+++ GoTorz/Model/Destination.cs
@@ -0,0 +1,11 @@
+﻿namespace GoTorz.Model
+{
+    public class Destination
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
